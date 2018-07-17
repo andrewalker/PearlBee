1c351388edcccc7fdd6e762215dc476d7d681c87
Andre Walker <andre@andrewalker.net>
2018-07-17 04:49:17 +0200

    Move business logic to the model

--- lib/PearlBee/API/Posts.pm
+++ lib/PearlBee/API/Posts.pm
@@ -4,14 +4,15 @@ package PearlBee::API::Posts;
 use Dancer2 appname => 'PearlBee';
 use Dancer2::Plugin::DBIC;
 use Dancer2::Plugin::Auth::Tiny;
-use Ref::Util qw(is_hashref is_ref is_blessed_scalarref is_arrayref);
-use Text::Unidecode ();
-use String::Truncate ();
 
-# TODO: move this to a more appropriate place
-use constant MAX_KEYS_POST_META  => 1000;
-use constant MAX_POST_TAGS       => 1000;
-use constant MAX_ABSTRACT_LENGTH => 1000;
+use PearlBee::Model::Posts;
+
+my $model = PearlBee::Model::Posts->new(
+    user_rs     => resultset('User'),
+    post_rs     => resultset('Post'),
+    post_tag_rs => resultset('PostTag'),
+    uri_for     => \&uri_for,
+);
 
 get '/api/posts' => sub {
     return api_posts_endpoint();
@@ -40,15 +41,15 @@ get '/api/user/posts' => sub {
 get '/api/posts/:id' => sub {
     my $post_id = route_parameters->{'id'};
 
-    my ($public_post) = search_posts({
-        id => $post_id
+    my ($public_post) = $model->search_posts({
+        id => $post_id,
     });
 
     $public_post and
         return send_as JSON => { post => $public_post };
 
     if (my $user = session 'user_id') {
-        my ($private_post) = search_posts({
+        my ($private_post) = $model->search_posts({
             id             => $post_id,
             author         => $user,
             only_published => 0,
@@ -85,42 +86,34 @@ post '/api/user/posts' => needs login => sub {
     }
 
     my $user_id = session 'user_id';
-    my $user    = resultset('User')->find($user_id);
     my $json    = decode_json( request->body );
 
-    if ($json->{meta}) {
-        is_hashref($json->{meta})
-            or return send_as_bad_request({ error => q/meta has to be a JSON object/ });
+    my ($post, $error) = $model->create_post($user_id, $json);
 
-        check_meta_deep($json->{meta})
-            or return send_as_bad_request({ error => q/meta can't have deep data structures/ });
+    for ( $error || () ) {
+        /^user-not-found$/
+            and return send_as_bad_request({ error => q/invalid user_id in session/ });
+        /^meta-not-json-object$/
+            and return send_as_bad_request({ error => q/meta has to be a JSON object/ });
+        /^meta-deep$/
+            and return send_as_bad_request({ error => q/meta can't have deep data structures/ });
+        /^meta-too-big$/
+            and return send_as_bad_request({ error => q/meta is too big/ });
+        /^tags-not-string-array$/
+            and return send_as_bad_request({ error => q/tags are supposed to be an array of strings/ });
+        /^tags-too-big$/
+            and return send_as_bad_request({ error => q/tags are too big/ });
+        /^abstract-too-big$/
+            and return send_as_bad_request({ error => q/abstract is too big/ });
+        /^post-not-created$/
+            and return send_as_bad_request({ error => q/unknown error creating post/ });
 
-        check_meta_size($json->{meta})
-            or return send_as_bad_request({ error => q/meta is too big/ });
+        return send_as_bad_request({ error => q/unknown error creating post/ });
     }
-
-    if ($json->{tags}) {
-        check_tags_format($json->{tags})
-            or return send_as_bad_request({ error => q/tags are supposed to be an array of strings/ });
-
-        check_tags_size($json->{tags})
-            or return send_as_bad_request({ error => q/tags are too big/ });
-    }
-
-    my $post = $user->add_to_posts({
-        title    => $json->{title},
-        slug     => sluggify($json->{slug} || $json->{title}),
-        abstract => abstractify($json->{abstract} || $json->{content}),
-        content  => $json->{content},
-        meta     => $json->{meta} ? encode_json($json->{meta}) : undef,
-        status   => $json->{status},
-    });
-
-    $post->add_to_post_tags({ tag => $_ }) for @{ $json->{tags} || [] };
 
     status 'created';
     send_as JSON => {
-        post => search_posts({ id => $post->id, published_only => 0 }),
+        post => $model->search_posts({ id => $post->id, published_only => 0 }),
     };
 };
 
@@ -133,125 +126,39 @@ patch '/api/posts/:id' => needs 'login' => sub {
     }
 
     my $user_id = session 'user_id';
-    my $post = resultset('Post')->find(route_parameters->{id});
+    my $post_id = route_parameters->{id};
+    my $json    = decode_json( request->body );
 
-    if (!$post) {
-        status 'not_found';
-        return send_as JSON => { error => 'not found' };
-    }
-    if (!$post->can_be_edited_by($user_id)) {
-        status 'forbidden';
-        return send_as JSON => { error => "You can't edit this post." };
-    }
+    my ($post, $error) = $model->update_post($user_id, $post_id, $json);
 
-    my $json = decode_json( request->body );
-
-    my $updated = 0;
-    my @post_columns = qw/title slug status content abstract/;
-    my %to_be_updated = map +($_, $json->{$_}),
-                        grep exists $json->{$_}, @post_columns;
-
-    if ($json->{meta}) {
-        is_hashref($json->{meta})
-            or return send_as_bad_request({ error => q/meta has to be a JSON object/ });
-
-        check_meta_deep($json->{meta})
-            or return send_as_bad_request({ error => q/meta can't have deep data structures/ });
-
-        check_meta_size($json->{meta})
-            or return send_as_bad_request({ error => q/meta is too big/ });
-
-        $to_be_updated{meta} = encode_json(
-            merge_patch( $post->meta ? decode_json( $post->meta ) : {}, $json->{meta} )
-        );
+    for ( $error || () ) {
+        if (/^post-not-found$/) {
+            status 'not_found';
+            return send_as JSON => { error => 'not found' };
+        }
+        if (/^forbidden$/) {
+            status 'forbidden';
+            return send_as JSON => { error => "You can't edit this post." };
+        }
+        /^meta-not-json-object$/
+            and return send_as_bad_request({ error => q/meta has to be a JSON object/ });
+        /^meta-deep$/
+            and return send_as_bad_request({ error => q/meta can't have deep data structures/ });
+        /^meta-too-big$/
+            and return send_as_bad_request({ error => q/meta is too big/ });
+        /^tags-not-string-array$/
+            and return send_as_bad_request({ error => q/tags are supposed to be an array of strings/ });
+        /^tags-too-big$/
+            and return send_as_bad_request({ error => q/tags are too big/ });
+        /^abstract-too-big$/
+            and return send_as_bad_request({ error => q/abstract is too big/ });
+        /^nothing-to-update$/
+            and return send_as_bad_request({ error => q/nothing was updated/ });
     }
 
-    if ($json->{tags}) {
-        check_tags_format($json->{tags})
-            or return send_as_bad_request({ error => q/tags are supposed to be an array of strings/ });
-
-        check_tags_size($json->{tags})
-            or return send_as_bad_request({ error => q/tags are too big/ });
-    }
-
-    if (%to_be_updated) {
-        $post->update(\%to_be_updated);
-        $updated++;
-    }
-    if (my $tags = $json->{tags}) {
-        $post->delete_related('tags');
-        $post->add_to_post_tags({ tag => $_ }) for @$tags;
-        $updated++;
-    }
-
-    if ($updated) {
-        $post->update({  updated_at => \'now()' });
-        status 'no_content';
-        return '';
-    }
-    else {
-        status 'bad_request';
-        send_as JSON => { error => 'Bad Request' };
-    }
+    status 'no_content';
+    return '';
 };
-
-
-#################################################################
-###                                                           ###
-### Helper methods                                            ###
-###                                                           ###
-#################################################################
-
-# TODO:
-# move these to a separate module
-sub check_meta_deep {
-    for (values %{ $_[0] }) {
-        # if it's a boolean, it will be a scalarref
-        return 0 if is_ref($_) && !is_blessed_scalarref($_);
-    }
-    return 1;
-}
-sub check_meta_size {
-    return keys %{ $_[0] } < MAX_KEYS_POST_META;
-}
-sub check_tags_format {
-    return 0 if !is_arrayref( $_[0] );
-    for (@{ $_[0] }) {
-        return 0 if is_ref($_);
-    }
-    return 1;
-}
-sub check_tags_size {
-    return @{ $_[0] } < MAX_POST_TAGS;
-}
-
-sub sluggify {
-    my ( $str )  = @_;
-    my $ldec_str = lc Text::Unidecode::unidecode($str);
-
-    return $ldec_str =~ s/[^0-9a-z]+/-/gr
-                     =~ s/^\-//gr
-                     =~ s/\-$//gr;
-}
-
-sub abstractify {
-    String::Truncate::elide($_[0], MAX_ABSTRACT_LENGTH, { at_space => 1 });
-}
-
-sub merge_patch {
-    my ($original, $patch) = @_;
-
-    for (keys %$patch) {
-        if (! defined $patch->{$_}) {
-            delete $original->{$_};
-        }
-        else {
-            $original->{$_} = $patch->{$_};
-        }
-    }
-
-    return $original;
-}
 
 sub send_as_bad_request {
     status 'bad_request';
@@ -274,7 +181,7 @@ sub api_posts_endpoint {
         $per_page = 50;
     }
 
-    my @posts = search_posts({
+    my @posts = $model->search_posts({
         author    => $author,
         per_page  => $per_page,
         page      => $page,
@@ -285,102 +192,6 @@ sub api_posts_endpoint {
     });
 
     send_as JSON => { posts => \@posts, };
-}
-
-# TODO: move this to the model
-sub search_posts {
-    my ($params) = @_;
-    my (@ids, @filter_query);
-
-    if ($params->{id}) {
-        @ids = $params->{id};
-    }
-    # XXX: This is complicated... if we're too permissive, we can open
-    # ourselves for DoS. We'll have to be careful when combining filters and
-    # tags. For now, we'll allow only one or the other. With the current
-    # implementation, this wouldn't be possible because we're limiting the rows
-    # for the tag search here.
-    elsif (my $tags = $params->{tags}) {
-        my @tags = split /,/, $tags;
-        # at most 3 tags
-        @tags = splice @tags, 0, 3;
-        @ids = map $_->{post_id}, search_tags(\@tags, $params->{per_page}, $params->{author});
-    }
-    # TODO: fulltext search
-    elsif (my $filter = $params->{'filter'}) {
-        @filter_query = (
-            -or => [
-                { 'me.title'    => { -ilike => "\%$filter\%" } },
-                { 'me.abstract' => { -ilike => "\%$filter\%" } },
-            ]
-        );
-    }
-
-    # Some endpoints can access all posts, others can only access the published
-    # posts. To be safe, the default is to show only the published ones.
-    my $only_published = exists $params->{only_published} ? $params->{only_published} : 1;
-    my @status_query
-        = $only_published
-        ? ( 'me.status' => 'published', 'author.verified_by_peers' => 1 )
-        : ();
-
-    my @paging_and_sorting = !$params->{id} ? (
-        order_by     => { $params->{direction} => $params->{sort} },
-        rows         => $params->{per_page},
-        page         => $params->{page},
-    ) : ();
-
-    my @id_query
-        = @ids == 1 ? ( 'me.id' => $ids[0] )
-        : @ids ? ( 'me.id' => { -in => \@ids } )
-        :        ();
-    my @author_query = $params->{author} ? ( 'me.author' => $params->{author} ) : ();
-
-    my @posts = map {
-        $_->{url} = uri_for('/' . $_->{author}{username} . '/' . $_->{slug});
-        $_->{tags} = [ sort map $_->{tag}, @{ delete $_->{post_tags} } ];
-        $_->{meta} = decode_json( delete $_->{meta} )
-            if $_->{meta};
-
-        # strip fractional seconds:
-        $_->{'created_at'} =~ s/(\.[0-9]+)(\+)/$2/;
-        $_->{'updated_at'} =~ s/(\.[0-9]+)(\+)/$2/;
-
-        $_;
-    } resultset('Post')->search(
-        { @status_query, @id_query, @filter_query, @author_query },
-        {
-            @paging_and_sorting,
-            join         => 'author',
-            prefetch     => 'post_tags',
-            '+select'    => ['author.username', 'author.name'],
-            result_class => 'DBIx::Class::ResultClass::HashRefInflator',
-        }
-    )->all;
-
-    return @posts;
-}
-
-# TODO: move this to the model
-sub search_tags {
-    my ($tags, $per_page, $author_id) = @_;
-
-    my $query
-        = @$tags == 1
-        ? { 'me.tag' => $tags->[0] }
-        : { -or => [ map +{ 'me.tag' => $_ }, @$tags ] };
-    my $options = {
-        columns      => ['post_id'],
-        result_class => 'DBIx::Class::ResultClass::HashRefInflator',
-        rows         => $per_page,
-    };
-
-    if ($author_id) {
-        $options->{'join'} = 'post';
-        $query->{'post.author'} = $author_id;
-    }
-
-    return resultset('PostTag')->search( $query, $options )->all;
 }
 
 1;
---
+++ lib/PearlBee/Model/Posts.pm
@@ -0,0 +1,309 @@
+package PearlBee::Model::Posts;
+
+use Moo;
+
+use Ref::Util qw(is_hashref is_ref is_blessed_scalarref is_arrayref);
+use Text::Unidecode ();
+use String::Truncate ();
+use JSON::MaybeXS;
+
+has uri_for => ( is => 'ro', required => 1 );
+
+has user_rs => (
+    is => 'ro',
+    required => 1,
+);
+
+has post_tag_rs => (
+    is => 'ro',
+    required => 1,
+);
+
+has post_rs => (
+    is => 'ro',
+    required => 1,
+);
+
+has max_keys_post_meta => (
+    is      => 'ro',
+    default => sub { 1000 },
+);
+
+has max_post_tags => (
+    is      => 'ro',
+    default => sub { 1000 },
+);
+
+has max_abstract_length => (
+    is      => 'ro',
+    default => sub { 1000 },
+);
+
+sub create_post {
+    my ($self, $user_id, $data) = @_;
+
+    my $user = $self->user_rs->find($user_id)
+        or return (undef, 'user-not-found');
+
+    if ($data->{meta}) {
+        is_hashref($data->{meta})
+            or return (undef, 'meta-not-json-object');
+
+        $self->check_meta_deep($data->{meta})
+            or return (undef, 'meta-deep');
+
+        $self->check_meta_size($data->{meta})
+            or return (undef, 'meta-too-big');
+    }
+
+    if ($data->{tags}) {
+        $self->check_tags_format($data->{tags})
+            or return (undef, 'tags-not-string-array');
+
+        $self->check_tags_size($data->{tags})
+            or return (undef, 'tags-too-big');
+    }
+
+    if ($data->{abstract}) {
+        $self->check_abstract_size($data->{abstract})
+            or return (undef, 'abstract-too-big');
+    }
+
+    my $post = $user->add_to_posts({
+        title    => $data->{title},
+        slug     => $self->sluggify($data->{slug} || $data->{title}),
+        abstract => $self->abstractify($data->{abstract} || $data->{content}),
+        content  => $data->{content},
+        meta     => $data->{meta} ? encode_json($data->{meta}) : undef,
+        status   => $data->{status},
+    });
+
+    $post
+        or return (undef, 'post-not-created');
+
+    $post->add_to_post_tags({ tag => $_ }) for @{ $data->{tags} || [] };
+
+    return ($post, undef);
+}
+
+sub update_post {
+    my ($self, $user_id, $post_id, $data) = @_;
+    my $post = $self->post_rs->find($post_id);
+
+    if (!$post) {
+        return (undef, 'post-not-found');
+    }
+    if (!$post->can_be_edited_by($user_id)) {
+        return (undef, 'forbidden');
+    }
+
+    my $updated = 0;
+    my @post_columns = qw/title slug status content abstract/;
+    my %to_be_updated = map +($_, $data->{$_}),
+                        grep exists $data->{$_}, @post_columns;
+
+    if ($data->{meta}) {
+        is_hashref($data->{meta})
+            or return (undef, 'meta-not-json-object');
+
+        $self->check_meta_deep($data->{meta})
+            or return (undef, 'meta-deep');
+
+        $self->check_meta_size($data->{meta})
+            or return (undef, 'meta-too-big');
+
+        $to_be_updated{meta} = encode_json(
+            merge_patch( $post->meta ? decode_json( $post->meta ) : {}, $data->{meta} )
+        );
+    }
+
+    if ($data->{tags}) {
+        $self->check_tags_format($data->{tags})
+            or return (undef, 'tags-not-string-array');
+
+        $self->check_tags_size($data->{tags})
+            or return (undef, 'tags-too-big');
+    }
+
+    if ($data->{abstract}) {
+        $self->check_abstract_size($data->{abstract})
+            or return (undef, 'abstract-too-big');
+    }
+
+    if (%to_be_updated) {
+        $post->update(\%to_be_updated);
+        $updated++;
+    }
+    if (my $tags = $data->{tags}) {
+        $post->delete_related('post_tags');
+        $post->add_to_post_tags({ tag => $_ }) for @$tags;
+        $updated++;
+    }
+
+    if (!$updated) {
+        return (undef, 'nothing-to-update');
+    }
+
+    $post->update({  updated_at => \'now()' });
+    return ($post, undef);
+}
+
+sub search_posts {
+    my ($self, $params) = @_;
+    my (@ids, @filter_query);
+
+    if ($params->{id}) {
+        @ids = $params->{id};
+    }
+    # XXX: This is complicated... if we're too permissive, we can open
+    # ourselves for DoS. We'll have to be careful when combining filters and
+    # tags. For now, we'll allow only one or the other. With the current
+    # implementation, this wouldn't be possible because we're limiting the rows
+    # for the tag search here.
+    elsif (my $tags = $params->{tags}) {
+        my @tags = split /,/, $tags;
+        # at most 3 tags
+        @tags = splice @tags, 0, 3;
+        @ids = map $_->{post_id}, $self->search_tags(\@tags, $params->{per_page}, $params->{author});
+    }
+    # TODO: fulltext search
+    elsif (my $filter = $params->{'filter'}) {
+        @filter_query = (
+            -or => [
+                { 'me.title'    => { -ilike => "\%$filter\%" } },
+                { 'me.abstract' => { -ilike => "\%$filter\%" } },
+            ]
+        );
+    }
+
+    # Some endpoints can access all posts, others can only access the published
+    # posts. To be safe, the default is to show only the published ones.
+    my $only_published = exists $params->{only_published} ? $params->{only_published} : 1;
+    my @status_query
+        = $only_published
+        ? ( 'me.status' => 'published', 'author.verified_by_peers' => 1 )
+        : ();
+
+    my @paging_and_sorting = !$params->{id} ? (
+        order_by     => { $params->{direction} => $params->{sort} },
+        rows         => $params->{per_page},
+        page         => $params->{page},
+    ) : ();
+
+    my @id_query
+        = @ids == 1 ? ( 'me.id' => $ids[0] )
+        : @ids ? ( 'me.id' => { -in => \@ids } )
+        :        ();
+    my @author_query = $params->{author} ? ( 'me.author' => $params->{author} ) : ();
+
+    my @posts = map {
+        $_->{url} = $self->uri_for->('/' . $_->{author}{username} . '/' . $_->{slug});
+        $_->{tags} = [ sort map $_->{tag}, @{ delete $_->{post_tags} } ];
+        $_->{meta} = decode_json( delete $_->{meta} )
+            if $_->{meta};
+
+        # strip fractional seconds:
+        $_->{'created_at'} =~ s/(\.[0-9]+)(\+)/$2/;
+        $_->{'updated_at'} =~ s/(\.[0-9]+)(\+)/$2/;
+
+        $_;
+    } $self->post_rs->search(
+        { @status_query, @id_query, @filter_query, @author_query },
+        {
+            @paging_and_sorting,
+            join         => 'author',
+            prefetch     => 'post_tags',
+            '+select'    => ['author.username', 'author.name'],
+            result_class => 'DBIx::Class::ResultClass::HashRefInflator',
+        }
+    )->all;
+
+    return @posts;
+}
+
+sub search_tags {
+    my ($self, $tags, $per_page, $author_id) = @_;
+
+    my $query
+        = @$tags == 1
+        ? { 'me.tag' => $tags->[0] }
+        : { -or => [ map +{ 'me.tag' => $_ }, @$tags ] };
+    my $options = {
+        columns      => ['post_id'],
+        result_class => 'DBIx::Class::ResultClass::HashRefInflator',
+        rows         => $per_page,
+    };
+
+    if ($author_id) {
+        $options->{'join'} = 'post';
+        $query->{'post.author'} = $author_id;
+    }
+
+    return $self->post_tag_rs->search( $query, $options )->all;
+}
+
+#################################################################
+###                                                           ###
+### Helper methods                                            ###
+###                                                           ###
+#################################################################
+
+# TODO:
+# move these to a separate module
+
+sub merge_patch {
+    my ($original, $patch) = @_;
+
+    for (keys %$patch) {
+        if (! defined $patch->{$_}) {
+            delete $original->{$_};
+        }
+        else {
+            $original->{$_} = $patch->{$_};
+        }
+    }
+
+    return $original;
+}
+sub check_meta_deep {
+    my ($self, $meta) = @_;
+    for (values %$meta) {
+        # if it's a boolean, it will be a scalarref
+        return 0 if is_ref($_) && !is_blessed_scalarref($_);
+    }
+    return 1;
+}
+sub check_meta_size {
+    my ($self, $meta) = @_;
+    return keys %$meta < $self->max_keys_post_meta;
+}
+sub check_abstract_size {
+    my ($self, $str) = @_;
+    return length $str < $self->max_abstract_length;
+}
+sub check_tags_format {
+    my ($self, $tags) = @_;
+    return 0 if !is_arrayref( $tags );
+    for (@$tags) {
+        return 0 if is_ref($_);
+    }
+    return 1;
+}
+sub check_tags_size {
+    my ($self, $tags) = @_;
+    return @$tags < $self->max_post_tags;
+}
+sub sluggify {
+    my ( $self, $str )  = @_;
+    my $ldec_str = lc Text::Unidecode::unidecode($str);
+
+    return $ldec_str =~ s/[^0-9a-z]+/-/gr
+                     =~ s/^\-//gr
+                     =~ s/\-$//gr;
+}
+sub abstractify {
+    my ($self, $str) = @_;
+    String::Truncate::elide($str, $self->max_abstract_length, { at_space => 1 });
+}
+
+1;
